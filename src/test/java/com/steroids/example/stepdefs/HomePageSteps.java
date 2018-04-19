43cd933fd144aa0791333e316483a729de9fa7bc
Tomasz Wojciechowski <wojcit01@stepstone.com>
2018-04-19 08:51:27 +0200

    starting repo

--- src/test/java/com/steroids/example/stepdefs/HomePageSteps.java
+++ src/test/java/com/steroids/example/stepdefs/HomePageSteps.java
@@ -1,36 +1,20 @@
 package com.steroids.example.stepdefs;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.steroids.example.framework.AbstractPage;
-import com.steroids.example.framework.MailNotFoundException;
 import com.steroids.example.pages.HomePage;
-import com.steroids.example.pages.JobAgentConfirmationEmailPage;
-import com.steroids.example.pages.PasswordSetPage;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-
-import java.io.IOException;
-import java.util.UUID;
 
 public class HomePageSteps {
 
   private WebDriver driver;
   private HomePage homePage;
-  private JobAgentConfirmationEmailPage jobAgentConfirmationEmailPage;
-  private String email;
-  private PasswordSetPage passwordSetPage;
-  private String subject;
+
 
   @Before(value = "@automated", order = 1)
   public void initWebDriver() {
@@ -63,61 +47,8 @@ public class HomePageSteps {
     homePage.navigateToHomePage();
   }
 
-  @When("^I perform search$")
-  public void theSearchPhraseIsEntered() {
-    homePage.performSearch();
-  }
-
-  @Then("^JobAgent Popover is shown$")
-  public void japoIsShown() {
-    assertThat(homePage.japoIsDisplayed()).isTrue();
-  }
-
-  @Then("^I type random email into JobAgent Popover$")
-  public void typeRandomEmailIntoJapo() {
-    email =
-        "test" + UUID.randomUUID().toString().substring(0, 10)
-        + "@ec2-34-244-6-12.eu-west-1.compute.amazonaws.com";
-    homePage.typeEmailToJapo(email);
-    AbstractPage.logInfo(email);
-    //TODO: Rozkminić jak ograć loginfo żeby nie odwoływać się do AbstractPage
-  }
-
-  @And("^I save JobAgent from JobAgent Popover")
-  public void saveJobAgentFromJapo() {
-    homePage.saveJobAgentFromJapo();
-  }
-
-  @Then("^Open JobAgent confirmation email$")
-  public void checkJobAgentConfirmationEmail() throws MailNotFoundException {
-    subject = "Bitte bestätigen Sie Ihren Job Agent - StepStone";
-    jobAgentConfirmationEmailPage = new JobAgentConfirmationEmailPage(driver);
-    jobAgentConfirmationEmailPage.goToConfirmationEmailFistep(email, subject);
-  }
-
-  @Then("^I check layout on \"([^\"]*)\"$")
-  public void checkLooksGood(String device) throws IOException {
-    jobAgentConfirmationEmailPage.confirmationEmailPageLayoutTest(driver);
-  }
-
-  @Then("^I check performance$")
-  public void checkPerf() {
-    homePage.checkPerformance();
-  }
-
   @After(value = "@automated")
   public void disposeWebDriver() {
     driver.quit();
-  }
-
-  @When("^I click confirmation button$")
-  public void iClickConfirmationButton() {
-    passwordSetPage = jobAgentConfirmationEmailPage
-        .confirmEmailMessageFistep(email, subject);
-  }
-
-  @Then("^I am on Password Set page$")
-  public void iAmOnPasswordSetPage() {
-    Assert.assertTrue(passwordSetPage.checkIfThisIsPasswordSetPage());
   }
 }
