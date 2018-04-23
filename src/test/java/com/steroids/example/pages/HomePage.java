b075b4580ffa4b32f0379efedc9fd2e8c3ce7b51
jaworj01 <jacek.jaworski@stepstone.com>
2018-04-23 11:43:32 +0200

    remove typo

--- src/test/java/com/steroids/example/pages/HomePage.java
+++ src/test/java/com/steroids/example/pages/HomePage.java
@@ -66,7 +66,7 @@ public class HomePage extends AbstractPage {
 
   public void saveJobAgentFromJapo() {
     getDriver().findElement(BY_JAPO_SAVE_BUTTON).click();
-  }git
+  }
 
   class Timings {
     private long loadEventStart;
