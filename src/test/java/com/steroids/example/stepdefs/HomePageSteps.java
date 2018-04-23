24d628767ead5de76c081709c975813ba59ab6c0
jaworj01 <jacek.jaworski@stepstone.com>
2018-04-23 21:04:44 +0200

    remove perf part

--- src/test/java/com/steroids/example/stepdefs/HomePageSteps.java
+++ src/test/java/com/steroids/example/stepdefs/HomePageSteps.java
@@ -99,11 +99,6 @@ public class HomePageSteps {
     jobAgentConfirmationEmailPage.confirmationEmailPageLayoutTest(driver, device);
   }
 
-  @Then("^I check performance$")
-  public void checkPerf() {
-    homePage.checkPerformance();
-  }
-
   @After(value = "@automated")
   public void disposeWebDriver() {
     driver.quit();
